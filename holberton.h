73d4bf21f924fcda5cca2b0e8c7466ab4ba47c0d
MelissaN <majajachiu@yahoo.com>
2018-02-25 04:53:00 +0000

    move struct declaration to print.c

--- holberton.h
+++ holberton.h
@@ -4,7 +4,6 @@
 #include <stdarg.h> /* va_list */
 #include <stdlib.h> /* malloc, free */
 #include <unistd.h> /* write */
-#include <string.h> /* strlen  --> write own!!!!!!!!!!!!!! */
 
 /* helper functions */
 char *_strcpy(char *dest, char *src);
@@ -20,13 +19,5 @@ typedef struct types
 	char id;
 	char* (*func)(va_list);
 } print;
-/*
-print keyss[] = {
-	{'c', print_c},
-	{'s', print_s},
-	{'d', print_d},
-	{'i', print_i},
-	{'\0', NULL}
-	};*/
 
 #endif
