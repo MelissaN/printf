da0d86d8a077aa3ea69bcc69d09a1ee8fcf66923
MelissaN <majajachiu@yahoo.com>
2018-02-25 05:40:48 +0000

    In Progress...
    Next steps:
    * handle buffer without mallocing?
    * copy function returns into buffer
    * relook at print functions without strcopy?

--- print.c
+++ print.c
@@ -3,26 +3,22 @@
 int _printf(const char *format, ...)
 {
 	int i = 0, j = 0, k = 0;
-	char *buffer;
 	va_list list;
+	char *buffer;
 
-	print keyss[] = {
+	print keys[] = {
 		{'c', print_c},
 		{'s', print_s},
        	/*	{'d', print_d},
 	      	{'i', print_i}, */
 		{'\0', NULL}
 	};
-
-	if (format == NULL) /* validate input */
+	buffer = malloc(1024);
+	if (buffer == NULL)
 		return (-1);
-	else
-	{
-		va_start(list, format);
-		buffer = malloc(10); /* malloc general size */
-		if (buffer == NULL) /* return if error */
-			return (-1);
-	}
+	if (format == NULL) /* validate input */
+		return (-2);
+       	va_start(list, format);
 
 	while (format[i] != '\0')
 	{
@@ -33,17 +29,17 @@ int _printf(const char *format, ...)
 		else /* if %, find and run function, assign to buffer */
 		{
 			i++;
-			while (keyss[k].id != '\0')
+			while (keys[k].id != '\0')
 			{
-				if (keyss[k].id == format[i])
-					keyss[k].func(list);
+				if (keys[k].id == format[i])
+					keys[k].func(list);
 				k++;
 			} i++;
 		}
 	}
-	buffer = realloc(buffer, strlen(buffer)); /* realloc to correct size */
-	write(1, buffer, strlen(buffer)); /* print */
+	buffer = realloc(buffer, _strlen(buffer)); /* realloc to correct size */
+	write(1, buffer, _strlen(buffer)); /* print */
 
 	va_end(list);
-	return (strlen(buffer));
+	return (_strlen(buffer));
 }
