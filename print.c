65a1d07b9a66246872365df3af81d2ba80983d5c
MelissaN <majajachiu@yahoo.com>
2018-02-25 20:45:26 +0000

    In progress... need to work on get_func

--- print.c
+++ print.c
@@ -1,39 +1,53 @@
 #include "holberton.h"
 
-int _printf(const char *format, ...)
+char* (*get_func(char i))(va_list)
 {
-	int i = 0, j = 0, k = 0;
-	va_list list;
-	char *buffer;
+	int k = 0;
 
 	print keys[] = {
-		{'c', print_c},
-		{'s', print_s},
-       	/*	{'d', print_d},
-	      	{'i', print_i}, */
-		{'\0', NULL}
-	};
+                {'c', print_c},
+                {'s', print_s},
+		/*      {'d', print_d},
+			{'i', print_i}, */
+                {'\0', NULL}
+        };
+
+	while (keys[k].id != '\0')
+	{
+		if (keys[k++].id == i)
+			return(keys[k].func);
+	}
+	return (NULL);
+}
+
+int _printf(const char *format, ...)
+{
+	int len = 0, i = 0;
+	va_list list;
+	char *buffer, *str;
+	char* (*f)(va_list);
+
+	if (format == NULL) /* validate input */
+		return (0);
 	buffer = malloc(1024);
 	if (buffer == NULL)
-		return (-1);
-	if (format == NULL) /* validate input */
-		return (-2);
+		return (0);
        	va_start(list, format);
 
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%') /* copy format into buffer until '%' */
+			buffer[len++] = format[i++];
+       		else /* if %, find and run function, assign to buffer */
 		{
-			buffer[j++] = format[i++];
-		}
-		else /* if %, find and run function, assign to buffer */
-		{
-			i++;
-			while (keys[k].id != '\0')
+			f = get_func(format[++i]);
+			if (f == NULL)
+				buffer[len++] = format[--i];
+			else
 			{
-				if (keys[k].id == format[i])
-					keys[k].func(list);
-				k++;
+				str = f(list);
+				while (*str != '\0')
+					buffer[len++] = *str++;
 			} i++;
 		}
 	}
