c30291644413599122b92116b4862566ca2de84b
MelissaN <majajachiu@yahoo.com>
2018-02-25 05:46:01 +0000

    remove string copy

--- string.c
+++ string.c
@@ -3,15 +3,8 @@
 char *print_s(va_list list)
 {
 	char *s;
-	char *p;
-	int strl;
 
 	s = va_arg(list, char *);
-	strl = _strlen(s);
 
-	p = malloc(sizeof(char) * strl + 1);
-	if (p == NULL)
-		return (NULL);
-
-	return (_strcpy(p,s));
+	return (s);
 }
