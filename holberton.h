82f8710e9452a55dccee77416681472fecc66743
MelissaN <majajachiu@yahoo.com>
2018-02-25 04:51:14 +0000

    add more protypes and struct

--- holberton.h
+++ holberton.h
@@ -2,9 +2,31 @@
 #define HOLBERTON_H
 
 #include <stdarg.h> /* va_list */
-#include <stdlib.h> /* malloc */
+#include <stdlib.h> /* malloc, free */
+#include <unistd.h> /* write */
+#include <string.h> /* strlen  --> write own!!!!!!!!!!!!!! */
 
+/* helper functions */
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+/* printf functions */
 int _printf(const char *format, ...);
+char *print_s(va_list list);
+char *print_c(va_list list);
 
+typedef struct types
+{
+	char id;
+	char* (*func)(va_list);
+} print;
+/*
+print keyss[] = {
+	{'c', print_c},
+	{'s', print_s},
+	{'d', print_d},
+	{'i', print_i},
+	{'\0', NULL}
+	};*/
 
 #endif
