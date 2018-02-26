76a6cea6616d86b9224e26edc7c0c2b4ee070a3f
Syssos <Syssos@gitgub.com>
2018-02-25 16:26:05 -0800

    account for null

--- char.c
+++ char.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
-
+/**
+ * print_c - returns char as string
+ * @list: char
+ * Return: String verion of char
+ */
 char *print_c(va_list list)
 {
 	char *s;
 
-	s = malloc(sizeof(char) * 2);
+	s = malloc(sizeof(char *) * 2);
+	if (s == NULL)
+		return (NULL);
 	s[0] = va_arg(list, int);
 	s[1] = '\0';
+	if (s == NULL)
+		return ("(null)");
 	return (s);
 }
--- string.c
+++ string.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
-
+/**
+ * print_s - takes string and return string
+ * @list: string
+ * Return: string
+ */
 char *print_s(va_list list)
 {
 	char *s;
 
 	s = va_arg(list, char *);
-
+	if (s == NULL)
+		return("(null)");
 	return (s);
 }
