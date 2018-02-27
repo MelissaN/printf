1face6da1ca938739aa812892b6eb3e418194d35
MelissaN <majajachiu@yahoo.com>
2018-02-27 00:17:09 +0000

    add more prototypes for helper functions

--- holberton.h
+++ holberton.h
@@ -6,8 +6,9 @@
 #include <unistd.h> /* write */
 
 /* helper functions */
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+char* (*get_func(char i))(va_list);
+char *create_buffer(void);
+void write_buffer(char *buffer, int len, va_list list);
 
 /* printf functions */
 int _printf(const char *format, ...);
