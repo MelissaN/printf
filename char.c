2951045f52e542ee79d2e773a060ca5eb43de586
MelissaN <majajachiu@yahoo.com>
2018-02-27 06:38:09 +0000

    check if va_arg is null first

--- char.c
+++ char.c
@@ -7,13 +7,18 @@
 char *print_c(va_list list)
 {
 	char *s;
+	char c;
+
+	c = va_arg(list, int);
+
+	if (c == 0)
+		c = '\0';
 
 	s = malloc(sizeof(char *) * 2);
 	if (s == NULL)
 		return (NULL);
-	s[0] = va_arg(list, int);
+	s[0] = c;
 	s[1] = '\0';
-	if (s == NULL)
-		return ("(null)");
+
 	return (s);
 }
