00aae8f2642d364b7eb51a4c70203a6ead139499
MelissaN <majajachiu@yahoo.com>
2018-02-27 00:18:02 +0000

    helper functions

---
+++ getFunc_and_buffer_functions.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+/**
+ * get_func - returns needed function
+ * @i: identifier for function
+ * Return: Pointer to needed function
+ */
+char* (*get_func(char i))(va_list)
+{
+	int k = 0;
+
+	print keys[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'d', print_d},
+		{'i', print_d},
+		{'\0', NULL}
+	};
+
+	while (keys[k].id != '\0')
+	{
+		if (keys[k].id == i)
+			return (keys[k].func);
+		k++;
+	}
+	return (NULL);
+}
+
+/**
+ * create_buffer - creates buffer to hold string until it's ready for print
+ * Return: pointer to buffer created
+ */
+char *create_buffer(void)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return (NULL);
+	return (buffer);
+}
+
+/**
+ * write_buffer - prints buffer, then frees it and frees va_list
+ * @buffer: buffer holding print-ables
+ * @len: length of print-able string
+ * @list: va_list
+ */
+void write_buffer(char *buffer, int len, va_list list)
+{
+	char *buff;
+
+	buff = realloc(buffer, len); /* realloc to correct size */
+	write(1, buff, len); /* print */
+
+	free(buff); va_end(list);
+}
