0488543ae9571b1919bea71872263724d59c6838
MelissaN <majajachiu@yahoo.com>
2018-08-30 21:36:57 +0000

    add examples of what printf can do in main program

--- print.c
+++ print.c
@@ -107,6 +107,18 @@ int _printf(const char *format, ...)
  */
 int main(void)
 {
-	_printf("%s %c%drld", "Hello", "W", 0);
+	_printf("\n\n\nHere's some examples of what you could do with this custom_printf function!\n\n\n");
+	sleep(1);
+	_printf("\nPrinting Strings, Characters, and Numbers...... %s %c%drld\n\n", "Hello", 'W', 0);
+	sleep(1);
+	_printf("Printing Reverse...... %r \n\n", "Hello");
+	sleep(1);
+	_printf("Printing Binary (base 2)...... %b \n\n", "Hello");
+	sleep(1);
+	_printf("Printing Octal (base 8)...... %o \n\n", "Hello");
+	sleep(1);
+	_printf("Printing Rot13 (encrypt)...... %R \n\n", "Hello");
+	sleep(1);
+	_printf("\n\n             = )                  \n\n\n");
 	return (0);
 }
